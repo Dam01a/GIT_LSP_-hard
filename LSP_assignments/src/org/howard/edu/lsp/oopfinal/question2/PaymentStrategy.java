3d0239c9f6591e16e64a70062474563377d9464d
Dam01a <damolaquadri5@gmail.com>
2024-05-02 19:22:20 -0400

    new new new

--- LSP_assignments/src/org/howard/edu/lsp/oopfinal/question2/PaymentStrategy.java
+++ LSP_assignments/src/org/howard/edu/lsp/oopfinal/question2/PaymentStrategy.java
@@ -59,19 +59,3 @@ public class ShoppingCart {
      paymentStrategy.pay(amount);
  }
 }
-
-//PaymentStrategyDriver.java
-public class PaymentStrategyDriver {
- public static void main(String[] args) {
-     ShoppingCart cart = new ShoppingCart();
-
-     cart.setPaymentStrategy(new CreditCardPayment("1234-5678-9012-3456"));
-     cart.checkout(100.0);
-
-     cart.setPaymentStrategy(new PayPalPayment("user@example.com"));
-     cart.checkout(50.0);
-
-     cart.setPaymentStrategy(new BitcoinPayment("1AaBbCcDdEeFfGgHh"));
-     cart.checkout(75.0);
- }
-}
